e1aaec61b090d8c955b0103b81b2f07135419de6
Nick Vas <nicksoft@abv.bg>
2022-05-26 16:15:08 +0300

    Finished Even Times

---
+++ Sets-and-Dictionaries-Advanced/Even Times/Even Times/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int magicNum = 0;
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (!numbers.ContainsKey(currentNum))
+                {
+                    numbers.Add(currentNum, 1);
+                }
+                else
+                    numbers[currentNum]++;
+            }
+            foreach (var KVP in numbers)
+            {
+                if (KVP.Value % 2 == 0)
+                {
+                    Console.WriteLine(KVP.Key);
+                }
+            }
+        }
+    }
+}
