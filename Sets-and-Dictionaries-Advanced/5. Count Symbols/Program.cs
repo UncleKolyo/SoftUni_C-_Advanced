bb07fa59bcc0ad76ae28654a7aa5efd196adaca5
Nick Vas <nicksoft@abv.bg>
2022-05-26 16:40:43 +0300

    Finished Count Symbols

---
+++ Sets-and-Dictionaries-Advanced/5. Count Symbols/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+namespace _5._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            char[] sortedInput = input.OrderBy(x => x).ToArray();
+
+            input = new string(sortedInput);
+
+            SortedDictionary<char, int> keyValuePairs = new SortedDictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                char currentChar = input[i];
+                if (!keyValuePairs.ContainsKey(currentChar))
+                {
+                    keyValuePairs.Add(currentChar, 1);
+                }
+                else
+                    keyValuePairs[currentChar]++;
+            }
+            foreach (var KVP in keyValuePairs)
+            {
+                Console.WriteLine($"{KVP.Key}: {KVP.Value} time/s");
+            }
+        }
+    }
+}
