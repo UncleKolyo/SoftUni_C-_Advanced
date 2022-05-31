541fe58a6be9dbe423e648c32e7a8e1d8bfcb081
Nick Vas <nicksoft@abv.bg>
2022-05-31 21:26:36 +0300

    Started FP.

---
+++ Functional Programming/FunctionalProgramming/Action Point/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Action_Point
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ').ToArray();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Action<string> print = message => Console.WriteLine(input[i]);
+                print(input[i]);
+            }
+        }
+    }
+}
