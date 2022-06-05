496935c0d4350515320b211ad12c5c5597e81d22
Nick Vas <nicksoft@abv.bg>
2022-06-05 15:26:51 +0300

    did a few more ex
    .

---
+++ Exercise Streams Files and Directories/Streams, Files and Directories/EvenLines/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+        public static string ProcessLines(string inputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                string output = "";
+                int counter = 1;
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        return output;
+                    }
+                    string[] reversing = line.Split(' ').ToArray();
+                    Array.Reverse(reversing);
+                    line = string.Join(" ", reversing);
+                    line = line.Replace('-', '@').Replace(',', '@').Replace('.', '@').Replace('!', '@').Replace('?', '@');
+                    if (counter % 2 == 0)
+                    {
+                        counter++;
+                        continue;
+                    }
+                    else
+                    {
+                        output += line + Environment.NewLine;
+                        counter++;
+                    }
+
+
+
+                }
+
+            }
+
+        }
+    }
+}
