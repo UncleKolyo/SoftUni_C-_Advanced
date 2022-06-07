d19fc66041ede3e2d776937bbdd3d0fb6ddbe4ec
Nick Vas <nicksoft@abv.bg>
2022-06-07 11:23:41 +0300

    Finished CopyBinaryFile

---
+++ Exercise Streams Files and Directories/Streams, Files and Directories/CopyBinaryFile/Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace CopyBinaryFile
+{
+    using System;
+    using System.IO;
+    public class CopyBinaryFile
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = @"..\..\..\copyMe.png";
+            string outputFilePath = @"..\..\..\copyMe-copy.png";
+            CopyFile(inputFilePath, outputFilePath);
+        }
+        public static void CopyFile(string inputFilePath, string outputFilePath)
+        {
+            using (var reader = new FileStream(inputFilePath, FileMode.Open))
+            {
+                using (var writer = new FileStream(outputFilePath, FileMode.Create))
+                {
+                    byte[] buffer = new byte[4096];
+
+                    var readBytes = 0;
+
+                    while ((readBytes = reader.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        writer.Write(buffer, 0, readBytes);
+                    }
+                }
+            }
+        }
+    }
+}
