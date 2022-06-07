e5d4190a623f9f87575120d15e06b6066fcb88e9
Nick Vas <nicksoft@abv.bg>
2022-06-07 09:51:23 +0300

    Finished Line numbers

--- Exercise Streams Files and Directories/Streams, Files and Directories/Line Numbers/Line Numbers/Program.cs
+++ Exercise Streams Files and Directories/Streams, Files and Directories/Line Numbers/Line Numbers/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.IO;
 
 namespace Line_Numbers
 {
-    internal class Program
+    public class LineNumbers
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string inputFilePath = @"..\..\..\input.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                int linecounter = 1;
+                int letterCounter = 0;
+                int punctCounter = 0;
+
+                while (line != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        char character = line[i];
+                        if (Char.IsLetter(character))
+                        {
+                            letterCounter++;
+                        }
+                        else if (Char.IsPunctuation(character))
+                        {
+                            punctCounter++;
+                        }
+                    }
+                  File.AppendAllText(outputFilePath, $"Line {linecounter}: {line} ({letterCounter}) ({punctCounter}) {Environment.NewLine}");
+                  //  Console.WriteLine($"Line {linecounter}: {line} ({letterCounter}) ({punctCounter})");
+                    letterCounter = 0;
+                    punctCounter = 0;
+                    linecounter++;
+                    line = reader.ReadLine();
+                }         
+            }
         }
     }
 }
---
+++ Exercise Streams Files and Directories/Streams, Files and Directories/Line Numbers/temp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace temp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            for (int i = 1; i < 30; i++)
+            {
+                Console.WriteLine($"a{i} = {a}");
+                a = a + 4;
+            }
+        }
+    }
+}
